05b9cca6e8bbf836eb71f890f5ab8d42e4d2592a
zhaobao1830 <x5219438>
2019-12-18 10:17:26 +0800

    把注解@Controller修改为@RestController

--- src/main/java/com/zb/missyou/api/v1/BannerController.java
+++ src/main/java/com/zb/missyou/api/v1/BannerController.java
@@ -1,18 +1,13 @@
 package com.zb.missyou.api.v1;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping(value = "/v1/banner")
 public class BannerController {
 
     @RequestMapping(value = "/test", method = RequestMethod.POST)
-    @ResponseBody
     public String test() {
-        return "Hello,zhaobao1830";
+        return "Hello,zhaobao18300";
     }
 }
