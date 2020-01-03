6b4cffd39137443fb7cef52403b21763229b300c
zhaobao1830 <x5219438>
2020-01-03 17:35:20 +0800

    把注解@Controller修改为@RestController

--- src/main/java/com/zb/missyou/api/v1/BannerController.java
+++ src/main/java/com/zb/missyou/api/v1/BannerController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(value = "/v1/banner")
 public class BannerController {
 
-    @RequestMapping(value = "/test", method = RequestMethod.POST)
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
     public String test() {
         return "Hello,zhaobao18300";
     }
