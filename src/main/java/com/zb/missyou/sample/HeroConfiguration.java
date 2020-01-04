c6f59771c7f480c1ed19adf2f865b00a9c239db1
zhaobao1830 <373098777@qq.com>
2020-01-04 22:53:59 +0800

    @Component和 @Autowired 一个接口多个类的实现 主动注入和被动注入

---
+++ src/main/java/com/zb/missyou/sample/HeroConfiguration.java
@@ -0,0 +1,13 @@
+package com.zb.missyou.sample;
+
+import com.zb.missyou.sample.hero.Camile;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HeroConfiguration {
+   @Bean
+    public ISkill camile() {
+        return new Camile();
+    }
+}
