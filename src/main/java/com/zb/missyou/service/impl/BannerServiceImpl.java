0e49cf5adfaba2b492bf1e58f186bab2dcabb361
zhaobao1830 <373098777@qq.com>
2020-02-05 11:58:07 +0800

    新建yml配置文件和BannerService接口和实现类

---
+++ src/main/java/com/zb/missyou/service/impl/BannerServiceImpl.java
@@ -0,0 +1,8 @@
+package com.zb.missyou.service.impl;
+
+import com.zb.missyou.service.BannerService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BannerServiceImpl implements BannerService {
+}
