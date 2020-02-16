c1e8f0c627ff66856e9fc2545d1ec340599e3c62
zhaobao1830 <373098777@qq.com>
2020-02-16 16:21:39 +0800

    mysql数据库连接配置

---
+++ src/main/java/com/zb/missyou/model/Banner.java
@@ -0,0 +1,12 @@
+package com.zb.missyou.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Banner {
+    private String id;
+    private String name;
+    private String description;
+    private String img;
+    private String title;
+}
