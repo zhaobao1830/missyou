01968cea274fb6e2d9bc24ecc53f083e832ccaf2
zhaobao1830 <x5219438>
2020-05-06 11:09:58 +0800

    修改spu实体类

--- src/main/java/com/zb/missyou/model/Spu.java
+++ src/main/java/com/zb/missyou/model/Spu.java
@@ -9,25 +9,23 @@ import java.util.Date;
 @Entity
 @Getter
 @Setter
-public class Spu {
+public class Spu extends BaseEntity{
     @Id
     private Long id;
     private String title;
     private String subtitle;
     private Long categoryId;
     private Integer rootCategoryId;
-    private byte online;
-    private Date createTime;
-    private Date updateTime;
-    private Date deleteTime;
+    private Boolean online;
     private String price;
-    private Integer sketchSpecId;
-    private Integer defaultSkuId;
+    private Long sketchSpecId;
+    private Long defaultSkuId;
     private String img;
     private String discountPrice;
     private String description;
     private String tags;
-    private Byte isTest;
-    private Object spuThemeImg;
+    private Boolean isTest;
+
+    // private Object spuThemeImg;
     private String forThemeImg;
 }
