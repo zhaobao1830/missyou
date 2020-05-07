ab1dc87fdf9572ffbb00d5a356312e9d32b82168
zhaobao1830 <x5219438>
2020-05-07 16:39:05 +0800

    将分页参数start、count转换为pageNum、count

---
+++ src/main/java/com/zb/missyou/util/CommonUtil.java
@@ -0,0 +1,15 @@
+﻿package com.zb.missyou.util;
+
+import com.zb.missyou.bo.PageCounter;
+
+public class CommonUtil {
+
+    public static PageCounter converToPageParameter(Integer start, Integer count){
+        int pageNum = start/count;
+        PageCounter pageCounter = PageCounter.builder()
+                .page(pageNum)
+                .count(count)
+                .build();
+        return pageCounter;
+    }
+}
