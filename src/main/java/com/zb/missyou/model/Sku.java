ba2e4e0712c44c0ad9063c2daff5759e114ed43b
zhaobao1830 <373098777@qq.com>
2020-05-14 09:52:32 +0800

    优化代码

--- src/main/java/com/zb/missyou/model/Sku.java
+++ src/main/java/com/zb/missyou/model/Sku.java
@@ -1,10 +1,12 @@
 package com.zb.missyou.model;
 
+import com.zb.missyou.util.MapAndjson;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
+import java.util.Map;
 
 @Entity
 @Getter
