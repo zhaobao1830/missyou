8d85628814c043b35bbbfe25f49b06da91f9ae75
zhaobao1830 <zgt..5219438>
2020-12-18 09:34:35 +0800

    删除测试代码

--- src/main/java/com/zb/missyou/api/v1/t/factory/Hero.java
+++
@@ -1,8 +0,0 @@
-package com.zb.missyou.api.v1.t.factory;
-
-public interface Hero {
-    void q();
-    void w();
-    void e();
-    void r();
-}
--- src/main/java/com/zb/missyou/api/v1/t/zero/Diana.java
+++
@@ -1,7 +0,0 @@
-package com.zb.missyou.api.v1.t.zero;
-
-public class Diana {
-    public void r() {
-        System.out.println("this is Diana");
-    }
-}
